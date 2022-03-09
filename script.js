4be80ca87b4b396928ff60742a2d005677caf14e
Chris Vazquez <cvg_v10@hotmail.com>
2022-03-08 22:42:01 -0800

    added script and added id to questions and options

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+
+currentQuestion = 0
+
+let questions = [
+  {
+    question: 'How do you pull a ID from HTML to Java?',
+    options: ['addEventListner','getElementById','pullId']
+    answer: 'getElementById'
+  },
+  {
+    question:'what do you use to style a page'
+    options:['CSS','HTML']
+    answer: 'CSS'
+
+  }
+]
+const displayQuestion = () => {
+  document.getElementById('question').innerHTML = `
+  ${questions[currentQuestion].questions}
+  `
+  
+
+  
+}
+
+
+
+
+document.getElementById('start').addEventListener('click'event =>{
+
+})
